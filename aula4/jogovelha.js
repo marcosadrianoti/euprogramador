50b3ebbff0ba2ce32bb1bc8677f0237ef0170b4f
Marcos Adriano <marcosadriano.ti@gmail.com>
2021-02-09 09:56:04 -0300

    jogo da velha versão 2

--- aula4/jogovelha.js
+++ aula4/jogovelha.js
@@ -18,56 +18,48 @@ Processamento
     Para cada jogador procura no array uma das 8 sequências vencederas.
 Fim
     Indica quem foi o vencedor.
+
+-------------------------------------------
+Sequẽncias vencedoras
+    Linhas
+        sequencia1 = [[0,0], [0,1], [0,2]];
+        sequencia2 = [[1,0], [1,1], [1,2]];
+        sequencia3 = [[2,0], [2,1], [2,2]];
+    Colunas
+        sequencia4 = [[0,0], [1,0], [2,0]];
+        sequencia5 = [[0,1], [1,1], [2,1]];
+        sequencia6 = [[0,2], [1,2], [2,2]];
+    Cruzados
+        sequencia7 = [[0,0], [1,1], [2,2]];
+        sequencia8 = [[2,0], [1,1], [0,2]];
 */
 
 // Resultado do jogo
 const jogo = [
-                [0, 1, 0],
-                [0, 0, 1],
-                [0, 1, 0]
+    [0, 1, 1],
+    [0, 0, 1],
+    [1, 0, 0]
 ];
 
-// Sequẽncias vencedoras
-    // Linhas
-    const sequencia1 = [[0,0], [0,1], [0,2]];
-    const sequencia2 = [[1,0], [1,1], [1,2]];
-    const sequencia3 = [[2,0], [2,1], [2,2]];
-    // Colunas
-    const sequencia4 = [[0,0], [1,0], [2,0]];
-    const sequencia5 = [[0,1], [1,1], [2,1]];
-    const sequencia6 = [[0,2], [1,2], [2,2]];
-    // Cruzados
-    const sequencia7 = [[0,0], [1,1], [2,2]];
-    const sequencia8 = [[2,0], [1,1], [0,2]];
-
+const sequenciasVencedoras = [
+    [ [ 0, 0 ], [ 0, 1 ], [ 0, 2 ] ],
+    [ [ 1, 0 ], [ 1, 1 ], [ 1, 2 ] ],
+    [ [ 2, 0 ], [ 2, 1 ], [ 2, 2 ] ],
+    [ [ 0, 0 ], [ 1, 0 ], [ 2, 0 ] ],
+    [ [ 0, 1 ], [ 1, 1 ], [ 2, 1 ] ],
+    [ [ 0, 2 ], [ 1, 2 ], [ 2, 2 ] ],
+    [ [ 0, 0 ], [ 1, 1 ], [ 2, 2 ] ],
+    [ [ 2, 0 ], [ 1, 1 ], [ 0, 2 ] ]
+    ];
 
 for (let jogador = 0; jogador <= 1; jogador++) {
-    if (procuraVencedor(sequencia1, jogador) == true) {
-        mostraVencedor(jogador);
-        break;
-        // console.log('teste');
-    }else if (procuraVencedor(sequencia2, jogador) == true) {
-        mostraVencedor(jogador);
-        break;
-    }else if (procuraVencedor(sequencia3, jogador) == true) {
-        mostraVencedor(jogador);
-        break;
-    }else if (procuraVencedor(sequencia4, jogador) == true) {
-        mostraVencedor(jogador);
-        break;
-    }else if (procuraVencedor(sequencia5, jogador) == true) {
-        mostraVencedor(jogador);
-        break;
-    }else if (procuraVencedor(sequencia6, jogador) == true) {
-        mostraVencedor(jogador);
-        break;
-    }else if (procuraVencedor(sequencia7, jogador) == true) {
-        mostraVencedor(jogador);
-        break;
-    }else if (procuraVencedor(sequencia8, jogador) == true) {
-        mostraVencedor(jogador);
-        break;
-    }
+    sequenciasVencedoras.forEach(
+        (sequencia) =>{
+            if (procuraVencedor(sequencia, jogador) == true) {
+                mostraVencedor(jogador);
+            }
+        }
+    )
 }
 
 function procuraVencedor(sequencia, jogador) {
@@ -78,14 +70,14 @@ function procuraVencedor(sequencia, jogador) {
                 jogadaChecada++
             }
         }
-        )
-        if (jogadaChecada == sequencia.length) {
-            venceu = true;
-        } else {
-            venceu = false;
-        }
-        return venceu;
+    )
+    if (jogadaChecada == sequencia.length) {
+        venceu = true;
+    } else {
+        venceu = false;
     }
+    return venceu;
+}
 
 function mostraVencedor(jogador) {
     console.log(`Vencedor = ${jogador}`)
