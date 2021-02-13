dbec73468838d2ce30aa7a97a8682c910a5a5f88
Marcos Adriano <marcosadriano.ti@gmail.com>
2021-02-13 13:33:21 -0300

    aula7-6.js started

---
+++ aula7/aula7-6.js
@@ -0,0 +1,6 @@
+/*
+Crie um array com datas de quando você foi à academia ou fez atividade
+fisica. Crie uma função para verficar se dado um array de datas, você fez
+atividade física nos ultimos sete dias.
+
+*/
