84e1d4988fa278f666071b4c19a0fa7163dd1b31
Marcos Adriano <marcosadriano.ti@gmail.com>
2021-02-13 14:06:30 -0300

    aula7-6 described process

--- aula7/aula7-6.js
+++ aula7/aula7-6.js
@@ -3,4 +3,15 @@ Crie um array com datas de quando você foi à academia ou fez atividade
 fisica. Crie uma função para verficar se dado um array de datas, você fez
 atividade física nos ultimos sete dias.
 
+ENTRADA
+    Recebe array com datas de quando fez atividade física
+PROCESSAMENTO
+    Pega cada data do array
+        Testa o seguinte:
+            Se a data faz parte dos últimos 7 dias então:
+                Indica que foi feita atividade fisica
+    Após a última data se nenhuma data apareceu nos últimos 7 dias então:
+        Indica que nenhuma atividade fisícia foi feita
+SAIDA
+    Mostra msg se fez ou não atividade física nos últimos 7 dias.
 */
