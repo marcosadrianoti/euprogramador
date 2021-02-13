39c98a6072e7691e8a6527dd3634bce1c7e1d34f
Marcos Adriano <marcosadriano.ti@gmail.com>
2021-02-13 09:23:17 -0300

    aula7-5.js started

---
+++ aula7/aula7-5.js
@@ -0,0 +1,14 @@
+/*
+Cria uma função que retorne o número de dias entre duas datas.
+
+ENTRADA
+    Recebe duas datas.
+PROCESSAMENTO
+    Converte as datas em milisegundos.
+    Calcula a diferença entre os valores em milisegundos.
+    Divide a diferença pelos milisegundos equivalentes a um dia.
+        um dia = 24 * 60 * 60 * 1000 = 86400000 milisegundos
+SAÍDA
+    Retorna o número de dias entre as duas datas.
+*/
+
