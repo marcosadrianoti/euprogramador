6d975ee9b6895a102fd2f02b18dde16350046d61
Marcos Adriano <marcosadriano.ti@gmail.com>
2021-02-15 19:58:52 -0300

    aula7-10.js done

--- aula7/aula7-10.js
+++ aula7/aula7-10.js
@@ -1,1 +1,15 @@
-/*Crie uma função para verificar se uma data é fim de semana ou não.*/
+/*
+Crie uma função para verificar se uma data é fim de semana ou não.
+*/
+
+const brDate = "07/02/2021";
+
+console.log(`É final de semana? ${isWeekend(brDate)}`);
+
+function isWeekend(date) {
+    const [day, month, year] = date.split("/");
+    const usDate = new Date(year, month-1, day);
+
+    return usDate.getDay() == '0' || usDate.getDay() == '6' ? 'SIM' : 'NÃO';
+    
+}
