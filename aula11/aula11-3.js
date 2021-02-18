cc91494f4c72ef916cda19929e2eeb574c2f243c
Marcos Adriano <marcosadriano.ti@gmail.com>
2021-02-18 16:55:08 -0300

    aula11-3.js started

---
+++ aula11/aula11-3.js
@@ -0,0 +1,8 @@
+/*
+Escreva uma função que recebe um array e retorna o último elemento do array
+se nenhum outro argumento for passado, porém se outro argumento for
+passado ela vai retornar os ultimos n elementos do array.
+console.log(first([1,2,3])) // => 3
+console.log(first([1,2,3], 2)) // => [2,3]
+*/
+
