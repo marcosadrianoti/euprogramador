c728ee53568292a576ba53da5eddfb852abaf53a
Marcos Adriano <marcosadriano.ti@gmail.com>
2021-02-18 16:47:52 -0300

    aula11-2 done

--- aula11/aula11-2.js
+++ aula11/aula11-2.js
@@ -1,5 +1,16 @@
+/*
 Escreva uma função que recebe um array e retorna o primeiro elemento do
 array se nenhum outro argumento for passado, porém se outro argumento for
 passado ela vai retornar os n primeiros elementos do array.
 console.log(first([1,2,3])) // => 1
 console.log(first([1,2,3], 2)) // => [1,2]
+*/
+
+const array = [1, 2, 3, 4, 5];
+
+console.log(first(array, 3));
+
+function first(array, nFirsts = 1) {
+    let newNFirsts = array.slice(0, nFirsts);
+    return newNFirsts;
+}
