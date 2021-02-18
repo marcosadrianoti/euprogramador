c3abb680b7567618c7506edf459edbaf6c0b28ee
Marcos Adriano <marcosadriano.ti@gmail.com>
2021-02-18 17:42:13 -0300

    aula11-4.js done

---
+++ aula11/aula11-4.js
@@ -0,0 +1,10 @@
+/*
+Escreva uma função que ordene os elementos de um array de ordem
+crescente.
+*/
+
+const array = [1,2,5,3,6,4];
+
+const crescent = (array) => array.sort();
+
+console.log(crescent(array));
