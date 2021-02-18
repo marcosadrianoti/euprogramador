534f83bae17f6458113a9a70c08e8536d7aa0b74
Marcos Adriano <marcosadriano.ti@gmail.com>
2021-02-18 15:00:54 -0300

    aula11-2.js estarted

---
+++ aula11/aula11-2.js
@@ -0,0 +1,5 @@
+Escreva uma função que recebe um array e retorna o primeiro elemento do
+array se nenhum outro argumento for passado, porém se outro argumento for
+passado ela vai retornar os n primeiros elementos do array.
+console.log(first([1,2,3])) // => 1
+console.log(first([1,2,3], 2)) // => [1,2]
