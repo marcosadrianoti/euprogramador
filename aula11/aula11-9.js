9c443d4e27d57207662ba3e6c0b5e1e9bfb60399
Marcos Adriano <marcosadriano.ti@gmail.com>
2021-02-19 14:44:17 -0300

    aula11-9.js started

---
+++ aula11/aula11-9.js
@@ -0,0 +1,12 @@
+/*
+Escreva uma função que cria um novo array com quantidade de elementos e o
+valor dados pelo usuário.
+console.log(fill(3, 'a')); // => ['a', 'a', 'a']
+console.log(fill(2, 'Marcle')); // => ['Marcle', 'Marcle']
+*/
+
+function fill(elements, value){
+
+
+
+}
