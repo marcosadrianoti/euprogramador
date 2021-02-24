42b70a50faec5f55660f50fd1b596127a178da58
Marcos Adriano <marcosadriano.ti@gmail.com>
2021-02-24 19:08:16 -0300

    aula14-1.js started

---
+++ aula14/aula14-1.js
@@ -0,0 +1,23 @@
+/*
+Escreva uma função em JavaScript que reverta um número.
+Exemplo: 1245
+Saida Esperada: 5421
+
+ENTRADA
+	Recebe um número.	
+PROCESSAMENTO
+	transforma o número recebido em string
+	convete a string em array
+	inverte o array
+	converte o array em string
+	tranforma a string em número
+SAIDA
+	Mostra no console o número invertido.
+*/
+
+const myNumber = 1245;
+
+function reverseNumber(){
+
+
+}
