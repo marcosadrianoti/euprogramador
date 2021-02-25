bb22d0d7bcdf30f255dbe3fdf1ecaf8991786316
Marcos Adriano <marcosadriano.ti@gmail.com>
2021-02-25 11:23:25 -0300

    aula14-2.js started

---
+++ aula14/aula14-2.js
@@ -0,0 +1,7 @@
+/*
+Escreva uma função em JavaScript que verifique se uma string passada é um
+palindromo ou não.
+Um palindromo é uma palavra, frase ou sequencia que é lida da mesma
+maneira de trás pra frente, por exemplo: "madam", "nurses run", "roma me tem
+amor".
+*/
