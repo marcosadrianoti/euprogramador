beb4617a2130b5a352fd5f59f84a599685ffd8ae
Marcos Adriano <marcosadriano.ti@gmail.com>
2021-02-26 13:01:51 -0300

    aula14-3.js started

---
+++ aula14/aula14-3.js
@@ -0,0 +1,27 @@
+/*
+Escreva uma função em JavaScript que retorna uma string passada com as
+letras em ordem alfabética. Assuma que pontuações e números não estão
+incluidas na string passada.
+Exemplo: "webmaster"
+Saida Esperada: "abeemrstw"
+
+function alphabeticalOrder(string) {
+  const withoutSpaces = string.replace(/ /g, '');
+
+  return withoutSpaces
+    .split('')
+    .sort()
+    .join('');
+}
+
+console.log(alphabeticalOrder('edcba'));
+console.log(alphabeticalOrder('cba'));
+console.log(alphabeticalOrder('webmaster'));
+*/
+
+const myString = "abeemrstw";
+
+function alphabeticalOrder(params) {
+    
+    
+}
