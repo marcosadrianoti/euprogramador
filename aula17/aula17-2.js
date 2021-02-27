f4c2584a4f65057d3d32355fcdee1e8f0b236537
Marcos Adriano <marcosadriano.ti@gmail.com>
2021-02-27 16:49:22 -0300

    aula17-2.js done

---
+++ aula17/aula17-2.js
@@ -0,0 +1,25 @@
+/*
+Escreva um programa em JavaScript que exclua uma propriedade de um objeto,
+imprimindo o objeto antes e depois de excluir a propriedade,
+const student = {
+    name: 'Marcle Rodrigues',
+    grade: 'A',
+    age: 26
+};
+deleteProperty(student, 'grade');
+*/
+
+const student = {
+    name: 'Marcle Rodrigues',
+    grade: 'A',
+    age: 26
+};
+
+deleteProperty(student, "grade");
+
+function deleteProperty(obj, property) {
+    console.log(obj);
+    delete obj.grade
+    console.log(obj);
+}
+
