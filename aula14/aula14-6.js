ddec5e224a723ce43b10ba78bcfcf65337954fe3
Marcos Adriano <marcosadriano.ti@gmail.com>
2021-02-27 09:46:28 -0300

    aula14-6.js started

---
+++ aula14/aula14-6.js
@@ -0,0 +1,33 @@
+/*
+6. Escreva uma função em JavaScript que extraia os caracteres unicos de uma
+string.
+Exemplo: "marclerodrigues"
+Saida Esperada: "marcleodigus"
+
+function unique(string) {
+  const set = new Set();
+
+  for(let i = 0; i < string.length; i++) {
+    const letter = string[i];
+
+    set.add(letter);
+  }
+
+  return [...set].join('');
+}
+
+console.log(unique('marclerodrigues'));
+console.log(unique('abcdef'));
+*/
+function unique(string) {
+  
+    for(let i = 0; i < string.length; i++) {
+      const letter = string[i];
+  
+    }
+  
+    return ;
+  }
+  
+  console.log(unique('marclerodrigues'));
+  console.log(unique('abcdef'));
