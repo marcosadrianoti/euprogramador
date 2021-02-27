e93a4945b75930bcc999213822629b70a8ada257
Marcos Adriano <marcosadriano.ti@gmail.com>
2021-02-27 10:30:39 -0300

    aula14-6.js done

--- aula14/aula14-6.js
+++ aula14/aula14-6.js
@@ -19,15 +19,29 @@ function unique(string) {
 console.log(unique('marclerodrigues'));
 console.log(unique('abcdef'));
 */
+
 function unique(string) {
-  
+    let newString = "";
+    let duplicatedLetter = false;
+
     for(let i = 0; i < string.length; i++) {
+
       const letter = string[i];
+
+            //Substitute for "Set" and "Join"
+            for(let id = 0; id < newString.length; id++) {
+
+                duplicatedLetter = newString[id] === letter ? true : false;
+                if (duplicatedLetter === true) {break};
+        
+            }
+
+        if (duplicatedLetter === false) {newString = newString + letter;}
   
     }
   
-    return ;
+    return newString;
   }
   
-  console.log(unique('marclerodrigues'));
-  console.log(unique('abcdef'));
+  console.log(unique('abacbdagbcadc'));
+  console.log(unique('Marcos Adriano Cruz'));
