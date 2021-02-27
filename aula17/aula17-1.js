4dd110778cf83e4779e526bb5235763bb3e708a3
Marcos Adriano <marcosadriano.ti@gmail.com>
2021-02-27 15:56:48 -0300

    aula17-1.js started
    commit initial for aula17-1

---
+++ aula17/aula17-1.js
@@ -0,0 +1,11 @@
+/*
+Escreva um programa em JavaScript que liste todas as propriedades de um
+objeto.
+const student = {
+name: 'Marcle Rodrigues',
+grade: 'A',
+age: 26
+};
+// => name, grade, age
+*/
+
