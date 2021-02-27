c29ffb5dbbd1cecec7b2b9503d5ee83422d32585
Marcos Adriano <marcosadriano.ti@gmail.com>
2021-02-27 16:16:10 -0300

    aula17-1 done

--- aula17/aula17-1.js
+++ aula17/aula17-1.js
@@ -9,3 +9,15 @@ age: 26
 // => name, grade, age
 */
 
+let list = "";
+const student = {
+    name: 'Marcle Rodrigues',
+    grade: 'A',
+    age: 26
+};
+
+for(property in Object(student)){
+    list = list + (list == "" ? property : `, ${property}`);
+}
+
+console.log(list);
