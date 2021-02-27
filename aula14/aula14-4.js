3197328adbf7c1ee40397e817b8937c1260e5d96
Marcos Adriano <marcosadriano.ti@gmail.com>
2021-02-27 09:21:08 -0300

    aula14-4.js done

--- aula14/aula14-4.js
+++ aula14/aula14-4.js
@@ -24,8 +24,27 @@ console.log(biggestWord('Marcle'));
 
 const myString = "Web Development Tutorial";
 
-
+console.log(biggestWord(myString));
 
 function biggestWord(myString) {
   
+  let biggestWord = "";
+  let words = [];
+  let initSub = 0;
+
+  //Substitute for ".split"
+  for (let i = 0; i <= myString.length; i++) {
+    if (myString[i]  == " " || i == myString.length){
+      words.push(myString.substring(initSub == 0 ? 0 : initSub, i));
+      initSub = i + 1;
+    }
+  }
+
+  for(let word of words) {
+    if (word.length > biggestWord.length) {
+      biggestWord = word;
+    }
+  }
+  
+  return biggestWord;
 }
