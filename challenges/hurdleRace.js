7de73b5cf84f91b5108edf726a7ac6cd51547498
Marcos Adriano <marcosadriano.ti@gmail.com>
2021-03-03 14:59:53 -0300

    arrow function to hurdleRace.js

--- challenges/hurdleRace.js
+++ challenges/hurdleRace.js
@@ -3,7 +3,7 @@
 console.log(hurdleRace(4, [1, 6, 3, 5, 2]))
 
 function hurdleRace(k, height) {
-    let max = height.reduce(function(a, b) {return Math.max(a, b)});
+    let max = height.reduce((a, b) => Math.max(a, b));
     let doses = max - k >= 0 ? max - k : 0;
     return doses;
 }
