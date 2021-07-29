de02fda571e57db97e46c3cf48162f3d251241ee
Marcos Adriano <marcosadriano.ti@gmail.com>
2021-07-29 17:56:31 -0300

    prof_aula2 started and studingJS

---
+++ studingJS/mymodule/app.js
@@ -0,0 +1,7 @@
+const { sub, soma } = require("./calcs");
+sub(2, 2);
+soma(2, 2);
+
+const teste = require("./calcs");
+teste.soma(5, 5);
+teste.sub(10, 3);
---
+++ studingJS/mymodule/calcs.js
@@ -0,0 +1,9 @@
+const soma = (a, b) => {
+  console.log(a + b);
+};
+
+const sub = (a, b) => {
+  console.log(a - b);
+};
+
+module.exports = { soma, sub };
